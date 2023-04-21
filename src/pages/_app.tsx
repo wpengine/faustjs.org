fd3482535167ecb11a0e634de6e5d61726af9811
Matthew Wright <matthew.wright@wpengine.com>
2023-04-21 11:27:12 -0500

    added init mui setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,30 @@
 import "../../faust.config";
 import React from "react";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import createEmotionCache from "../utility/createEmotionCache";
+import lightTheme from "../styles/theme/lightTheme";
 import { useRouter } from "next/router";
 import { FaustProvider } from "@faustwp/core";
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+const clientSideEmotionCache = createEmotionCache();
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ 
+  Component, 
+  pageProps, 
+  emotionCache = clientSideEmotionCache 
+}: AppProps | any) {
   const router = useRouter();
 
   return (
     <FaustProvider pageProps={pageProps}>
-      <Component {...pageProps} key={router.asPath} />
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} key={router.asPath} />
+        </ThemeProvider>
+      </CacheProvider>
     </FaustProvider>
   );
 }
