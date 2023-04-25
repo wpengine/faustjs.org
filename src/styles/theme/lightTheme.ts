75fa6ac0f770b5f153ff2d8f8aa565e7572945e5
Matthew Wright <matthew.wright@wpengine.com>
2023-04-25 10:59:22 -0500

    Updated Theme

--- src/styles/theme/lightTheme.ts
+++ src/styles/theme/lightTheme.ts
@@ -3,6 +3,28 @@ import { createTheme } from '@mui/material/styles';
 const lightTheme = createTheme({
   palette: {
     mode: 'light',
+    primary: {
+      main: '#ffffff',
+      contrastText: '#002838',
+    },
+    secondary: {
+      main: '#7a1ba6',
+    },
+    text: {
+      primary: '#002838',
+      secondary: '#002838',
+      disabled: 'rgba(0,40,45,0.38)',
+
+    },
+    background: {
+      default: '#ffffff',
+    },
+    error: {
+      main: '#d21b46',
+    },
+  },
+  typography: {
+    fontFamily: 'Open Sans, tahoma, sans-serif',
   },
 });
 
