eb33455f14d7ca17eee0ed3a093c3e9e059a8d85
Matthew Wright <matthew.wright@wpengine.com>
2023-05-03 09:26:13 -0500

    Update content.tsx

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -3,7 +3,7 @@ import { Grid, Typography } from "@mui/material";
 
 export default function Content() {
   return (
-    <Grid container spacing={2} xs={12}>
+    <Grid container spacing={2}>
       <Typography>Content</Typography>
     </Grid>
   );
