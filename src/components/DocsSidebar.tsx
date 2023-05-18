b33592f70abd32d66fdcdac13cddd555733a8a98
Matthew Wright <matthew.wright@wpengine.com>
2023-05-18 12:15:40 -0500

    added drawer sidebar

--- src/components/DocsSidebar.tsx
+++ src/components/DocsSidebar.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { flatListToHierarchical } from '@faustwp/core';
+import {
+  Box,
+  Drawer,
+  Toolbar,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  List,
+  ListItemButton,
+  Divider,
+  Collapse,
+} from '@mui/material';
 import { DocsSidebarMenuItemFragmentFragment } from '__generated__/graphql';
 import { gql } from '../__generated__';
+
+const drawerWidth = 240;
 
 type DocsSidebarProps = {
   menuItems: DocsSidebarMenuItemFragmentFragment[];
@@ -10,34 +25,84 @@ type DocsSidebarProps = {
 
 export function DocsSidebar(props: DocsSidebarProps) {
   const { menuItems } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [container, setContainer] = React.useState(null);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   const hierarchicalMenuItems = flatListToHierarchical(menuItems);
 
   function renderMenu(items: Array<{}>) {
     return (
-      <ul>
+      <List>
         {items.map((item: any) => {
           const { id, path, label, children } = item;
 
           return (
-            <li key={id}>
-              <Link href={path ?? ''}>{label ?? ''}</Link>
-              {children.length ? renderMenu(children) : null}
-            </li>
+            <>
+              <Link key={id} href={path} style={{ flex: 1 }}>
+                <ListItemButton component="div">
+                  <ListItemText>{label}</ListItemText>
+                </ListItemButton>
+              </Link>
+              {children && children.length ? (
+                <Collapse sx={{ ml: 2 }} timeout="auto" unmountOnExit>
+                  {renderMenu(children)}
+                </Collapse>
+              ) : null}
+            </>
           );
         })}
-      </ul>
+      </List>
     );
   }
 
+  const drawer = <div>{renderMenu(menuItems)}</div>;
+
+  useEffect(() => {
+    setContainer(window !== undefined ? () => window.document.body : undefined);
+  }, []);
+
   return (
-    <aside>
-      <nav
-        role="navigation"
-        aria-label={`${menuItems[0]?.menu?.node?.name} menu`}>
-        {renderMenu(hierarchicalMenuItems)}
-      </nav>
-    </aside>
+    <Box sx={{ display: 'flex' }}>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}>
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+          open>
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
   );
 }
 
