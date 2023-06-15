ba3f0c081c8aefda248f319a6b234c9a16fbe883
ruqmuse <ruqayyah.muse@wpengine.com>
2023-06-14 23:06:40 -0700

    Split into components; updated the onHover color

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,107 +1,42 @@
 import React from 'react';
 import { Box, Button, Grid, Typography } from '@mui/material';
-import Image from 'next/image';
+import { HeroBanner } from './HeroBanner';
+import { FeatureBlock } from './FeatureBlock';
 
 export function Content() {
   return (
-    <Grid>
+    <Grid container>
       {/* Hero Banner */}
-      <Grid container spacing={2}>
-        <Grid item xs={12} sx={{ textAlign: 'center' }}>
-          <Image
-            src="public/icons/placeholder.svg"
-            alt="Faust.js Logo"
-            width={100}
-            height={100}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h3" sx={{ textAlign: 'center' }}>
-            Faust.js
-          </Typography>
-        </Grid>
+      <HeroBanner />
+      {/* Code Snippet + Code Result Grid Container */}
+      <Grid container spacing={2} sx={{ mt: 3 }}>
+        <FeatureBlock
+          title="Faust Toolbar"
+          codeSnippet="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          codeResult="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          altBlockColor="#ebedf063"
+        />
 
-        <Grid item xs={12}>
-          <Typography variant="body1" sx={{ textAlign: 'center' }}>
-            A JavaScript framework that makes building headless WordPress simple
-            and easy.
-          </Typography>
-        </Grid>
+        <FeatureBlock
+          title="Gutenberg Block Editor"
+          codeSnippet="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          codeResult="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          altBlockColor=""
+        />
 
-        <Grid item xs={12} sx={{ textAlign: 'center' }}>
-          {/* Any special effects, such as onHover? */}
-          <Button
-            href="/tutorial/get-started-with-faust"
-            variant="contained"
-            sx={{
-              backgroundColor: 'var(--faust--footer-background-color)',
-              color: 'white',
-            }}>
-            Get Started
-          </Button>
-        </Grid>
-      </Grid>
+        <FeatureBlock
+          title="Plugin System"
+          codeSnippet="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          codeResult="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          altBlockColor="#ebedf063"
+        />
 
-      {/* Code Snippet + Code Result Grid Large Container */}
-      <Grid container spacing={2}>
-        {/* Feature Block */}
-        <Grid container spacing={2} sx={{ m: 5 }}>
-          {/* Title */}
-          <Grid item xs={12}>
-            <Typography variant="body1" sx={{ textAlign: 'left' }}>
-              Faust Toolbar
-            </Typography>
-          </Grid>
-          {/* Code Snippet */}
-          <Grid
-            item
-            xs={12}
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-around',
-            }}>
-            <Box
-              sx={{
-                width: '50%',
-                height: 'auto',
-                my: 1,
-                mr: 2,
-                p: 2,
-                pb: 2.5,
-                bgcolor: 'var(--faust--footer-background-color)',
-                color: 'white',
-              }}>
-              <Typography>Code Snippet</Typography>
-              <Typography variant="body2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Adipisci voluptatum, temporibus dicta non illo iure! Expedita
-                dolor nesciunt iure iste quibusdam deleniti, consectetur
-                commodi, sit corrupti debitis, quaerat dolorem officia.
-              </Typography>
-            </Box>
-            {/* Code Result */}
-            <Box
-              sx={{
-                width: '50%',
-                height: 'auto',
-                my: 1,
-                ml: 2,
-                p: 2,
-                pb: 2.5,
-                border: '1px solid',
-                borderColor: 'var(--faust--footer-background-color)',
-              }}>
-              <Typography>Code Result</Typography>
-              <Typography variant="body2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Adipisci voluptatum, temporibus dicta non illo iure! Expedita
-                dolor nesciunt iure iste quibusdam deleniti, consectetur
-                corrupti debitis, quaerat dolorem officia.
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
+        <FeatureBlock
+          title="WP Templates"
+          codeSnippet="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          codeResult="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          altBlockColor=""
+        />
       </Grid>
     </Grid>
   );
---
+++ src/components/FeatureBlock.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+
+export function FeatureBlock({
+  title,
+  codeSnippet,
+  codeResult,
+  altBlockColor,
+}: {
+  title: string;
+  codeSnippet: string;
+  codeResult: string;
+  altBlockColor: string;
+}) {
+  return (
+    <Grid
+      container
+      spacing={1}
+      sx={{ m: 0, p: 2, backgroundColor: altBlockColor }}>
+      {/* Title */}
+      <Grid
+        item
+        xs={12}
+        sx={{
+          p: 1,
+          display: 'flex',
+          flexDirection: 'row',
+          pb: 0,
+        }}>
+        <Typography variant="body1" sx={{ textAlign: 'left' }}>
+          {title}
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          p: 1,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+        }}>
+        {/* Code Snippet */}
+        <Box
+          sx={{
+            width: '50%',
+            height: 'auto',
+            my: 1,
+            mr: 2,
+            p: 2,
+            pb: 2.5,
+            bgcolor: 'var(--faust--footer-background-color)',
+            color: 'white',
+          }}>
+          <Typography>Code Snippet</Typography>
+          <Typography variant="body2">{codeSnippet}</Typography>
+        </Box>
+        {/* Code Result */}
+        <Box
+          sx={{
+            width: '50%',
+            height: 'auto',
+            my: 1,
+            ml: 2,
+            p: 2,
+            pb: 2.5,
+            border: '1px solid',
+            borderColor: 'var(--faust--footer-background-color)',
+          }}>
+          <Typography>Code Result</Typography>
+          <Typography variant="body2">{codeResult}</Typography>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
