6d86024b1238d864735c196bfb6841a5b4e78171
ruqmuse <ruqayyah.muse@wpengine.com>
2023-06-16 08:47:12 -0700

    Added breakpoints for responsive styling

--- src/components/FeatureBlock.tsx
+++ src/components/FeatureBlock.tsx
@@ -48,6 +48,10 @@ export function FeatureBlock({
           display: 'flex',
           flexDirection: 'row',
           justifyContent: 'space-around',
+          '@media screen and (max-width: 600px)': {
+            flexDirection: 'column',
+            justifyContent: 'center',
+          },
         }}>
         {/* Code Snippet */}
         <Box
@@ -60,6 +64,7 @@ export function FeatureBlock({
             pb: 2.5,
             bgcolor: 'var(--faust--footer-background-color)',
             color: 'white',
+            '@media screen and (max-width: 600px)': { width: '100%' },
           }}>
           <Typography>{codeSnippetTitle}</Typography>
           <Typography variant="body2">{codeSnippet}</Typography>
@@ -75,6 +80,7 @@ export function FeatureBlock({
             pb: 2.5,
             border: '1px solid',
             borderColor: 'var(--faust--footer-background-color)',
+            '@media screen and (max-width: 600px)': { width: '100%', ml: 0 },
           }}>
           <Typography>{codeResultTitle}</Typography>
           {isImage ? (
