bef082772d8001606410d9fe65d23b30c0df7960
ruqmuse <ruqayyah.muse@wpengine.com>
2023-06-16 09:44:27 -0700

    Updated styling to reduce empty space in left card

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,14 +6,14 @@ import { FeatureBlock } from './FeatureBlock';
 
 export function Content() {
   return (
-    <Grid container>
+    <Grid container sx={{ m: 0 }}>
       {/* Hero Banner */}
       <HeroBanner />
       {/* Code Snippet + Code Result Grid Container */}
       <Grid container spacing={2} sx={{ mt: 3 }}>
         <FeatureBlock
           title="Faust Toolbar"
-          codeSnippetTitle="Code Snippet"
+          // codeSnippetTitle="Code Snippet"
           codeSnippet="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
           isImage={false}
           codeResultTitle="Code Result"
@@ -23,8 +23,8 @@ export function Content() {
 
         <FeatureBlock
           title="Gutenberg Block Editor"
-          codeSnippetTitle=""
-          codeSnippet="Regain a drag-and-drop WYSIWYG editing experience in a headless WordPress environment."
+          // codeSnippetTitle=""
+          codeSnippet="Regain a drag-and-drop WYSIWYG editing experience in a headless WordPress environment.\n\nFaust's blocks package provides the tools needed to match or extend the UI for Gutenberg’s block editing experience in headless."
           isImage
           codeResultTitle="How It Works:"
           // eslint-disable-next-line global-require, import/no-absolute-path
@@ -34,8 +34,8 @@ export function Content() {
 
         <FeatureBlock
           title="Plugin System"
-          codeSnippetTitle="Code Snippet"
-          codeSnippet="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          // codeSnippetTitle="Code Snippet"
+          codeSnippet="Harness a plugin ecosystem that allows folks to build flexibly to meet their goals."
           isImage={false}
           codeResultTitle="Code Result"
           codeResult="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
@@ -44,7 +44,7 @@ export function Content() {
 
         <FeatureBlock
           title="WP Templates"
-          codeSnippetTitle="Code Snippet"
+          // codeSnippetTitle="Code Snippet"
           codeSnippet="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
           isImage={false}
           codeResultTitle="Code Result"
--- src/components/FeatureBlock.tsx
+++ src/components/FeatureBlock.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export function FeatureBlock({
   title,
-  codeSnippetTitle,
+  //   codeSnippetTitle,
   codeSnippet,
   isImage,
   codeResultTitle,
@@ -12,7 +12,7 @@ export function FeatureBlock({
   altBlockColor,
 }: {
   title: string;
-  codeSnippetTitle: string;
+  //   codeSnippetTitle: string;
   codeSnippet: string;
   isImage: boolean;
   codeResultTitle: string;
@@ -56,18 +56,23 @@ export function FeatureBlock({
         {/* Code Snippet */}
         <Box
           sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
             width: '50%',
             height: 'auto',
             my: 1,
             mr: 2,
-            p: 2,
-            pb: 2.5,
+            p: 5,
+            // pb: 2.5,
             bgcolor: 'var(--faust--footer-background-color)',
             color: 'white',
             '@media screen and (max-width: 600px)': { width: '100%' },
           }}>
-          <Typography>{codeSnippetTitle}</Typography>
-          <Typography variant="body2">{codeSnippet}</Typography>
+          {/* <Typography>{codeSnippetTitle}</Typography> */}
+          <Typography variant="body1" style={{ whiteSpace: 'pre-line' }}>
+            {codeSnippet.replace(/\\n/g, '\n')}
+          </Typography>
         </Box>
         {/* Code Result */}
         <Box
@@ -77,16 +82,15 @@ export function FeatureBlock({
             my: 1,
             ml: 2,
             p: 2,
-            pb: 2.5,
             border: '1px solid',
             borderColor: 'var(--faust--footer-background-color)',
             '@media screen and (max-width: 600px)': { width: '100%', ml: 0 },
           }}>
-          <Typography>{codeResultTitle}</Typography>
+          <Typography sx={{ pb: 1 }}>{codeResultTitle}</Typography>
           {isImage ? (
             <Image src={codeResult} alt="" style={{ width: '100%' }} />
           ) : (
-            <Typography variant="body2">{codeResult}</Typography>
+            <Typography variant="body1">{codeResult}</Typography>
           )}
         </Box>
       </Grid>
