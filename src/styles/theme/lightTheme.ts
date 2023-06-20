5d9694548b7d7f69add2c8a534def1b3814c287a
Teresa (Terri) Gobble <teresa.gobble@wpengine.com>
2023-06-19 22:06:16 -0500

    Removed dark background from home page

--- src/styles/theme/lightTheme.ts
+++ src/styles/theme/lightTheme.ts
@@ -15,9 +15,6 @@ const lightTheme = createTheme({
       secondary: '#002838',
       disabled: 'rgba(0,40,45,0.38)',
     },
-    background: {
-      default: '#002838',
-    },
     error: {
       main: '#d21b46',
     },
