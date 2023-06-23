44507f782f6d75c9ec475db5c908b009c67642de
Matthew Wright <matthew.wright@wpengine.com>
2023-06-23 10:47:12 -0500

    updated logic checks

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -30,111 +30,116 @@ export function Posts(props: PostsProps) {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        {posts.map((post: Post, i: number) => {
-          const getFormattedDate = (date: string) => {
-            const dateObj = new Date(date);
-            const dateMDY = `${dateObj.toLocaleString('default', {
-              month: 'long',
-            })} ${dateObj.getMonth() + 1}, ${dateObj.getFullYear()}`;
-            return dateMDY;
-          };
+        {posts &&
+          posts?.length > 0 &&
+          posts.map((post: Post, i: number) => {
+            const getFormattedDate = (date: string) => {
+              const dateObj = new Date(date);
+              const dateMDY = `${dateObj.toLocaleString('default', {
+                month: 'long',
+              })} ${dateObj.getMonth() + 1}, ${dateObj.getFullYear()}`;
+              return dateMDY;
+            };
 
-          return (
-            <>
-              <hr style={{ opacity: 0.25 }} />
-
-              <Box
-                key={post.id}
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  my: 6,
-                }}>
-                <Typography
-                  variant="body1"
-                  component="h6"
-                  sx={{
-                    mb: 2,
-                    color: 'rgb(107 114 128)',
-                    textAlign: 'center',
-                  }}>
-                  {getFormattedDate(post.date)}
-                </Typography>
-                <Typography
-                  variant="h4"
-                  component="h3"
-                  sx={{
-                    mb: 2,
-                    fontWeight: 'bold',
-                    color: '#663DEC',
-                    textAlign: 'center',
-                    '&:hover': {
-                      color: '#663DEC',
-                      textDecoration: 'underline',
-                    },
-                  }}>
-                  <Link
-                    href={post.uri}
-                    style={{
-                      color: '#7e5cef',
-                      textDecoration: 'none',
-                    }}>
-                    {post.title}
-                  </Link>
-                </Typography>
-
-                {post.tags.nodes.length > 0 && (
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                    }}>
-                    <ListItem
-                      disablePadding
-                      disableGutters
-                      sx={{ justifyContent: 'center', mb: 2 }}>
-                      {post.tags.nodes.map((tag: any) => {
-                        return (
-                          <Chip
-                            label={tag.name}
-                            sx={{ mr: 1 }}
-                            onClick={(event) => handleTagClick(event, tag.link)}
-                          />
-                        );
-                      })}
-                    </ListItem>
-                  </Box>
-                )}
+            return (
+              <>
+                <hr style={{ opacity: 0.25 }} />
 
                 <Box
+                  key={post.id}
                   sx={{
                     display: 'flex',
                     flexDirection: 'column',
-                    mb: 4,
-                  }}
-                  dangerouslySetInnerHTML={{
-                    __html: post.excerpt,
-                  }}
-                />
-                <Button
-                  href={post.uri}
-                  variant="contained"
-                  size="medium"
-                  sx={{
-                    alignSelf: 'center',
-                    maxWidth: '150px',
-                    '&:hover': {
-                      backgroundColor: '#663DEC',
-                    },
-                    backgroundColor: '#7e5cef',
-                    color: 'white',
+                    my: 6,
                   }}>
-                  Read More
-                </Button>
-              </Box>
-            </>
-          );
-        })}
+                  <Typography
+                    variant="body1"
+                    component="h6"
+                    sx={{
+                      mb: 2,
+                      color: 'rgb(107 114 128)',
+                      textAlign: 'center',
+                    }}>
+                    {getFormattedDate(post.date)}
+                  </Typography>
+                  <Typography
+                    variant="h4"
+                    component="h3"
+                    sx={{
+                      mb: 2,
+                      fontWeight: 'bold',
+                      color: '#663DEC',
+                      textAlign: 'center',
+                      '&:hover': {
+                        color: '#663DEC',
+                        textDecoration: 'underline',
+                      },
+                    }}>
+                    <Link
+                      href={post.uri}
+                      style={{
+                        color: '#7e5cef',
+                        textDecoration: 'none',
+                      }}>
+                      {post.title}
+                    </Link>
+                  </Typography>
+
+                  {post?.tags?.nodes?.length > 0 && (
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                      }}>
+                      <ListItem
+                        disablePadding
+                        disableGutters
+                        sx={{ justifyContent: 'center', mb: 2 }}>
+                        {post.tags.nodes.map((tag: any) => {
+                          return (
+                            <Chip
+                              key={tag.id}
+                              label={tag.name}
+                              sx={{ mr: 1 }}
+                              onClick={(event) =>
+                                handleTagClick(event, tag.link)
+                              }
+                            />
+                          );
+                        })}
+                      </ListItem>
+                    </Box>
+                  )}
+
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      mb: 4,
+                    }}
+                    dangerouslySetInnerHTML={{
+                      __html: post.excerpt,
+                    }}
+                  />
+                  <Button
+                    href={post.uri}
+                    variant="contained"
+                    size="medium"
+                    sx={{
+                      alignSelf: 'center',
+                      maxWidth: '150px',
+                      '&:hover': {
+                        backgroundColor: '#663DEC',
+                      },
+                      backgroundColor: '#7e5cef',
+                      color: 'white',
+                    }}>
+                    Read More
+                  </Button>
+                </Box>
+              </>
+            );
+          })}
         {posts && posts?.length < 1 && <p>No posts found.</p>}
       </Grid>
     </Grid>
@@ -156,6 +161,7 @@ Posts.fragments = {
       }
       tags {
         nodes {
+          id
           name
           link
         }
