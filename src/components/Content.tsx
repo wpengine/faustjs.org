9aae6f26663bd3af53d2586becf606b72a081f3f
ruqmuse <ruqayyah.muse@wpengine.com>
2023-06-26 09:40:53 -0700

    Added Toolbar image and edited page styling

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -12,43 +12,40 @@ export function Content() {
       {/* Code Snippet + Code Result Grid Container */}
       <Grid container spacing={2} sx={{ mt: 3 }}>
         <FeatureBlock
-          title="Faust Toolbar"
-          // codeSnippetTitle="Code Snippet"
+          codeSnippetTitle="Faust Toolbar"
           codeSnippet="A familiar publisher experience, providing a cohesive experience with wp-admin.\n\nEasily extensible with built in filters, empowering developers to customize for different use cases."
-          isImage={false}
-          codeResultTitle="Code Result"
-          codeResult="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          isImage
+          // eslint-disable-next-line global-require, import/no-absolute-path
+          codeResult={require('/public/images/ToolbarImage.png')}
+          altSide={false}
           altBlockColor=""
         />
 
         <FeatureBlock
-          title="Gutenberg Block Editor"
-          // codeSnippetTitle=""
+          codeSnippetTitle="Gutenberg Block Editor"
           codeSnippet="Regain a drag-and-drop WYSIWYG editing experience in a headless WordPress environment.\n\nFaust's blocks package provides the tools needed to match or extend the UI for Gutenberg’s block editing experience in headless."
           isImage
-          codeResultTitle="How It Works:"
           // eslint-disable-next-line global-require, import/no-absolute-path
-          codeResult={require('/public/images/GutenbergBlockImage.png')}
+          codeResult={require('/public/images/GutenbergBlocks.png')}
+          altSide={false}
           altBlockColor=""
         />
 
         <FeatureBlock
-          title="Plugin System"
-          // codeSnippetTitle="Code Snippet"
+          codeSnippetTitle="Plugin System"
           codeSnippet="Harness a plugin ecosystem that allows folks to build flexibly to meet their goals."
           isImage={false}
-          codeResultTitle="Code Result"
           codeResult="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          altSide={false}
           altBlockColor=""
         />
 
         <FeatureBlock
-          title="WP Templates"
-          // codeSnippetTitle="Code Snippet"
+          codeSnippetTitle="WP Templates"
           codeSnippet="Closely implement the behavior of the WordPress Template Hierarchy via a Javascript/GraphQL frontend configuration."
           isImage={false}
-          codeResultTitle="Code Result"
           codeResult="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum, temporibus dicta non illo iure! Expedita dolor nesciuntiure iste quibusdam deleniti, consectetur commodi, sit corrupti debitis, quaerat dolorem officia."
+          altSide={false}
           altBlockColor=""
         />
       </Grid>
