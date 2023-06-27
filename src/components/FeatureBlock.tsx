982b24dcfa97e45882774519125c0cd1b5f0ba3e
ruqmuse <ruqayyah.muse@wpengine.com>
2023-06-26 19:48:46 -0700

    Updated toolbar image

--- src/components/FeatureBlock.tsx
+++ src/components/FeatureBlock.tsx
@@ -17,123 +17,183 @@ export function FeatureBlock({
   altSide: boolean;
   altBlockColor: string;
 }) {
-  let returnItem;
-  if (altSide) {
-    returnItem = (
-      <Grid
-        item
-        xs={12}
+  // let returnItem;
+  // if (altSide) {
+  //   returnItem = (
+  //     <Grid
+  //       item
+  //       xs={12}
+  //       sx={{
+  //         p: 1,
+  //         display: 'flex',
+  //         flexDirection: 'row',
+  //         justifyContent: 'space-around',
+  //         '@media screen and (max-width: 600px)': {
+  //           flexDirection: 'column',
+  //           justifyContent: 'center',
+  //         },
+  //       }}>
+  //       {/* Code Result */}
+  //       <Box
+  //         sx={{
+  //           width: '50%',
+  //           height: 'auto',
+  //           borderRadius: '4px',
+  //           my: 1,
+  //           ml: 2,
+  //           p: 2,
+  //           '@media screen and (max-width: 600px)': { width: '100%', ml: 0 },
+  //         }}>
+  //         {isImage ? (
+  //           <Image src={codeResult} alt="" style={{ width: '100%' }} />
+  //         ) : (
+  //           <Typography variant="body1">{codeResult}</Typography>
+  //         )}
+  //       </Box>
+  //       {/* Code Snippet */}
+  //       <Box
+  //         sx={{
+  //           display: 'flex',
+  //           justifyContent: 'center',
+  //           flexDirection: 'column',
+  //           width: '50%',
+  //           height: 'auto',
+  //           borderRadius: '4px',
+  //           my: 1,
+  //           mr: 2,
+  //           p: 5,
+  //           bgcolor: 'var(--wp--preset--color--contrast)',
+  //           color: 'white',
+  //           '@media screen and (max-width: 600px)': { width: '100%' },
+  //         }}>
+  //         <Typography sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
+  //           {codeSnippetTitle}
+  //         </Typography>
+  //         <br />
+  //         <Typography variant="body1" style={{ whiteSpace: 'pre-line' }}>
+  //           {codeSnippet.replace(/\\n/g, '\n')}
+  //         </Typography>
+  //       </Box>
+  //     </Grid>
+  //   );
+  // } else {
+  //   returnItem = (
+  //     <Grid
+  //       item
+  //       xs={12}
+  //       sx={{
+  //         p: 1,
+  //         display: 'flex',
+  //         flexDirection: 'row',
+  //         justifyContent: 'space-around',
+  //         '@media screen and (max-width: 600px)': {
+  //           flexDirection: 'column',
+  //           justifyContent: 'center',
+  //         },
+  //       }}>
+  //       {/* Code Snippet */}
+  //       <Box
+  //         sx={{
+  //           display: 'flex',
+  //           justifyContent: 'center',
+  //           flexDirection: 'column',
+  //           width: '50%',
+  //           height: 'auto',
+  //           borderRadius: '4px',
+  //           my: 1,
+  //           mr: 2,
+  //           p: 5,
+  //           bgcolor: 'var(--wp--preset--color--contrast)',
+  //           color: 'white',
+  //           '@media screen and (max-width: 600px)': { width: '100%' },
+  //         }}>
+  //         <Typography sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
+  //           {codeSnippetTitle}
+  //         </Typography>
+  //         <br />
+  //         <Typography variant="body1" style={{ whiteSpace: 'pre-line' }}>
+  //           {codeSnippet.replace(/\\n/g, '\n')}
+  //         </Typography>
+  //       </Box>
+  //       {/* Code Result */}
+  //       <Box
+  //         sx={{
+  //           width: '50%',
+  //           height: 'auto',
+  //           borderRadius: '4px',
+  //           my: 1,
+  //           ml: 2,
+  //           p: 2,
+  //           '@media screen and (max-width: 600px)': { width: '100%', ml: 0 },
+  //         }}>
+  //         {isImage ? (
+  //           <Image src={codeResult} alt="" style={{ width: '100%' }} />
+  //         ) : (
+  //           <Typography variant="body1">{codeResult}</Typography>
+  //         )}
+  //       </Box>
+  //     </Grid>
+  //   );
+  // }
+  return (
+    <Grid
+      item
+      xs={12}
+      sx={{
+        p: 1,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        '@media screen and (max-width: 600px)': {
+          flexDirection: 'column',
+          justifyContent: 'center',
+        },
+      }}>
+      {/* Code Snippet */}
+      <Box
         sx={{
-          p: 1,
           display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          '@media screen and (max-width: 600px)': {
-            flexDirection: 'column',
-            justifyContent: 'center',
-          },
+          justifyContent: 'center',
+          flexDirection: 'column',
+          width: '50%',
+          height: 'auto',
+          borderRadius: '4px',
+          my: 1,
+          mr: 2,
+          p: 5,
+          // pb: 2.5,
+          bgcolor: 'var(--wp--preset--color--contrast)',
+          color: 'white',
+          '@media screen and (max-width: 600px)': { width: '100%' },
         }}>
-        {/* Code Result */}
-        <Box
-          sx={{
-            width: '50%',
-            height: 'auto',
-            borderRadius: '4px',
-            my: 1,
-            ml: 2,
-            p: 2,
-            '@media screen and (max-width: 600px)': { width: '100%', ml: 0 },
-          }}>
-          {isImage ? (
-            <Image src={codeResult} alt="" style={{ width: '100%' }} />
-          ) : (
-            <Typography variant="body1">{codeResult}</Typography>
-          )}
-        </Box>
-        {/* Code Snippet */}
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            width: '50%',
-            height: 'auto',
-            borderRadius: '4px',
-            my: 1,
-            mr: 2,
-            p: 5,
-            bgcolor: 'var(--wp--preset--color--contrast)',
-            color: 'white',
-            '@media screen and (max-width: 600px)': { width: '100%' },
-          }}>
-          <Typography sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
-            {codeSnippetTitle}
-          </Typography>
-          <br />
-          <Typography variant="body1" style={{ whiteSpace: 'pre-line' }}>
-            {codeSnippet.replace(/\\n/g, '\n')}
-          </Typography>
-        </Box>
-      </Grid>
-    );
-  } else {
-    returnItem = (
-      <Grid
-        item
-        xs={12}
+        <Typography sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
+          {codeSnippetTitle}
+        </Typography>
+        <br />
+        <Typography variant="body1" style={{ whiteSpace: 'pre-line' }}>
+          {codeSnippet.replace(/\\n/g, '\n')}
+        </Typography>
+      </Box>
+      {/* Code Result */}
+      <Box
         sx={{
-          p: 1,
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          '@media screen and (max-width: 600px)': {
-            flexDirection: 'column',
-            justifyContent: 'center',
-          },
+          width: '50%',
+          height: 'auto',
+          borderRadius: '4px',
+          my: 1,
+          ml: 2,
+          p: 2,
+          // border: '1px solid',
+          // borderColor: 'var(--wp--preset--color--contrast)',
+          '@media screen and (max-width: 600px)': { width: '100%', ml: 0 },
         }}>
-        {/* Code Snippet */}
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            width: '50%',
-            height: 'auto',
-            borderRadius: '4px',
-            my: 1,
-            mr: 2,
-            p: 5,
-            bgcolor: 'var(--wp--preset--color--contrast)',
-            color: 'white',
-            '@media screen and (max-width: 600px)': { width: '100%' },
-          }}>
-          <Typography sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
-            {codeSnippetTitle}
-          </Typography>
-          <br />
-          <Typography variant="body1" style={{ whiteSpace: 'pre-line' }}>
-            {codeSnippet.replace(/\\n/g, '\n')}
-          </Typography>
-        </Box>
-        {/* Code Result */}
-        <Box
-          sx={{
-            width: '50%',
-            height: 'auto',
-            borderRadius: '4px',
-            my: 1,
-            ml: 2,
-            p: 2,
-            '@media screen and (max-width: 600px)': { width: '100%', ml: 0 },
-          }}>
-          {isImage ? (
-            <Image src={codeResult} alt="" style={{ width: '100%' }} />
-          ) : (
-            <Typography variant="body1">{codeResult}</Typography>
-          )}
-        </Box>
-      </Grid>
-    );
-  }
-  return <Grid>{returnItem}</Grid>;
+        {/* <Typography sx={{ pb: 1 }}>{codeResultTitle}</Typography> */}
+        {isImage ? (
+          <Image src={codeResult} alt="" style={{ width: '100%' }} />
+        ) : (
+          <Typography variant="body1">{codeResult}</Typography>
+        )}
+      </Box>
+    </Grid>
+  );
 }
