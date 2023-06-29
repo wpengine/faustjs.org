b8c53a016119bd57b5004508874982e97c3c521a
Joe Fusco <joe.fusco@wpengine.com>
2023-06-29 11:54:01 -0400

    Resolve merge conflicts
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- src/wp-templates/SingleFaustReference.tsx
+++ src/wp-templates/SingleFaustReference.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head';
 import { FaustTemplate } from '@faustwp/core';
 import { Container, Grid } from '@mui/material';
 import { gql } from '__generated__';
@@ -7,7 +6,14 @@ import {
   DocsSidebarMenuItemsFragmentFragment,
   GetReferenceQuery,
 } from '__generated__/graphql';
-import { Header, Footer, EntryHeader, SidebarLayout, Main } from 'components';
+import {
+  Head,
+  Header,
+  Footer,
+  EntryHeader,
+  SidebarLayout,
+  Main,
+} from 'components';
 
 const Component: FaustTemplate<GetReferenceQuery> = (props) => {
   const { loading, data } = props;
@@ -33,9 +39,7 @@ const Component: FaustTemplate<GetReferenceQuery> = (props) => {
 
   return (
     <>
-      <Head>
-        <title>{`${title} - ${siteTitle}`}</title>
-      </Head>
+      <Head title={`${title} - ${siteTitle}`} />
 
       <Header
         siteTitle={siteTitle}
