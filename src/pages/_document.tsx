a0643ddaad9913e1025d930917360885c1c3c67b
Matthew Wright <matthew.wright@wpengine.com>
2023-06-29 11:42:47 -0500

    added ignore for prettier and dangeroussetinnerhtml

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,8 +14,8 @@ export default class MyDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_KEY}`}
           />
-          <script
-            dangerouslySetInnerHTML={{
+          {/* eslint-disable-next-line react/no-danger, prettier/prettier  */}
+          <script dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
