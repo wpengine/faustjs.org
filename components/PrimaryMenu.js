386a716d94e108d9b324eb55f3de3d160f568bf0
Fran Agulto <francis.agulto@wpengine.com>
2024-07-29 14:09:29 -0500

    added logic and query to PrimaryMenu.js file to get menu items from WP backend

--- components/PrimaryMenu.js
+++ components/PrimaryMenu.js
@@ -1,10 +1,44 @@
+import { gql, useQuery } from "@apollo/client";
 import Link from "next/link";
 
 export default function PrimaryMenu() {
+  const { data, loading, error } = useQuery(GET_PRIMARY_NAV);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.error(error);
+    return <p>Error loading navigation items.</p>;
+  }
+
+  const menuItems = data?.menu?.menuItems?.nodes || [];
+
   return (
     <nav className="flex gap-5">
-      <Link href="/docs">Docs</Link>
-      <Link href="/blog">Blog</Link>
+      {menuItems.length > 0 ? (
+        menuItems.map((item) => (
+          <Link key={item.databaseId} href={item.uri}>
+            {item.label}
+          </Link>
+        ))
+      ) : (
+        <p>No menu items found</p>
+      )}
     </nav>
   );
 }
+
+const GET_PRIMARY_NAV = gql`
+  query GetPrimaryNav {
+    menu(id: "primary-nav", idType: NAME) {
+      id
+      name
+      menuItems {
+        nodes {
+          label
+          uri
+          databaseId
+        }
+      }
+    }
+  }
+`;
