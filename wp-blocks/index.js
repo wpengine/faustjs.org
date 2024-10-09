efb21be6e1e251de62f17ef386fdd1c4098aadbe
Alex Moon <moonmeister@users.noreply.github.com>
2024-10-09 08:51:27 -0700

    nvmrc, corepack packagemanager, bugfixes

--- wp-blocks/index.js
+++ wp-blocks/index.js
@@ -1,9 +1,11 @@
 import { CoreBlocks } from "@faustwp/blocks";
 import CoreHeading from "./CoreHeading";
+import { CoreQuote } from "./CoreQuote";
 import KevinbatdorfCodeBlockPro from "./KevinbatdorfCodeBlockPro";
 
 export default {
   ...CoreBlocks,
   CoreHeading,
+  CoreQuote,
   KevinbatdorfCodeBlockPro,
 };
