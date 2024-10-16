ad8c29b982938542780d77a5b3e5b172b125dc30
Kellen Mace <kellen.mace@gmail.com>
2024-10-16 15:35:41 -0400

    Remove wp templates related to docs

--- wp-templates/index.js
+++ wp-templates/index.js
@@ -1,7 +1,5 @@
 import page from "./page";
 import single from "./single";
-import DocsArchive from "./docs-archive";
-import SingleDoc from "./single-doc";
 import IndexTemplate from "./IndexTemplate";
 import BlogIndex from "./blog-page";
 
@@ -9,7 +7,5 @@ export default {
   index: IndexTemplate,
   page,
   single,
-  "archive-doc": DocsArchive,
-  "single-doc": SingleDoc,
   "page-blog-page": BlogIndex,
 };
