508e99364cb88eaccffd83053f51c432b0174ccc
Damon Cook <colorful-tones@users.noreply.github.com>
2024-10-25 11:18:40 -0400

    Use @next/font/local to pull in Inter/Lora fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,25 @@
 import Header from "./header";
 import Footer from "./footer";
 
+import localFont from "next/font/local";
+
+const inter = localFont({
+	src: "/fonts/inter-variable-font.woff2",
+	display: "swap",
+	variable: "--font-inter",
+});
+
+const lora = localFont({
+	src: "/fonts/lora-variable-font.woff2",
+	display: "swap",
+	variable: "--font-lora",
+});
+
 export default function Layout({ children }) {
 	return (
-		<div className="flex min-h-screen flex-col">
+		<div
+			className={`${inter.variable} ${lora.variable} flex min-h-screen flex-col`}
+		>
 			<Header />
 			<main className="container mx-auto my-16 flex-grow px-4 sm:px-6 md:max-w-6xl md:px-8">
 				{children}
