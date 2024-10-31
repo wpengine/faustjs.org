52ed67680b354d9b4881e35f156b0624b84ec3cc
Damon Cook <colorful-tones@users.noreply.github.com>
2024-10-31 17:07:39 -0400

    Add Discord button and general styling enhancements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import {
+	ArrowTopRightOnSquareIcon,
 	ChevronRightIcon,
 	CodeBracketIcon,
 	CursorArrowRaysIcon,
 	KeyIcon,
 	RectangleGroupIcon,
-} from "@heroicons/react/16/solid";
+} from "@heroicons/react/24/outline";
 import Link from "@/components/link";
 
 // The Component is required
@@ -22,16 +23,28 @@ export default function Index() {
 								A robust set of tools for Next.js that unlock powerful headless
 								WordPress capabilities.
 							</p>
-							<p>
+							<p className="not-prose inline-flex gap-4">
 								<Link
 									noDefaultStyles
-									className="not-prose group inline-flex items-center rounded-full border border-gray-200/20 bg-gradient-to-br from-purple-600/20 from-10% to-blue-600/20 px-8 py-2 text-base leading-none tracking-tight text-gray-200 shadow-lg backdrop-blur-lg transition duration-300 hover:border-gray-100/50 hover:bg-gradient-to-tl hover:from-gray-100/70 hover:to-white/70 hover:text-gray-900 hover:backdrop-blur-xl lg:py-3"
+									className="not-prose text-gray-1000 group inline-flex items-center gap-1 rounded-full border border-blue-800/20 bg-gradient-to-tr from-blue-600/50 from-20% to-blue-900/50 px-8 py-2 text-base leading-none tracking-tight shadow-lg backdrop-blur-lg transition duration-75 hover:border-gray-100/50 hover:bg-gradient-to-tl hover:from-gray-100/70 hover:to-white/70 hover:text-gray-900 hover:backdrop-blur-xl lg:py-3"
 									href="/docs/"
 								>
 									Read the Docs
 									<ChevronRightIcon
 										focusable="false"
-										className="transition-duration-150 group-hover:fill-gray-1000 inline h-6 transition group-hover:translate-x-1"
+										className="group-hover:fill-gray-1000 inline h-5 w-5 transition group-hover:translate-x-1"
+									/>
+								</Link>
+								<Link
+									noDefaultStyles
+									disableExternalIcon
+									className="not-prose group inline-flex items-center gap-1 rounded-full border border-gray-200/20 bg-gradient-to-br from-purple-600/20 from-10% to-blue-600/20 px-8 py-2 text-base leading-none tracking-tight text-gray-200 shadow-lg backdrop-blur-lg transition duration-75 hover:border-gray-100/50 hover:bg-gradient-to-tl hover:from-gray-100/70 hover:to-white/70 hover:text-gray-900 hover:backdrop-blur-xl lg:py-3"
+									href="https://discord.gg/J2khkF9XYK"
+								>
+									Join the Discord
+									<ArrowTopRightOnSquareIcon
+										focusable="false"
+										className="group-hover:fill-gray-1000 inline h-5 transition group-hover:translate-x-1"
 									/>
 								</Link>
 							</p>
@@ -102,13 +115,13 @@ export default function Index() {
 						<p className="mt-16 text-center">
 							<Link
 								noDefaultStyles
-								className="not-prose group inline-flex items-center rounded-full border border-gray-200/20 bg-gradient-to-br from-purple-600/20 from-10% to-blue-600/20 px-8 py-2 text-base leading-none tracking-tight text-gray-200 shadow-lg backdrop-blur-lg transition duration-300 hover:border-gray-100/50 hover:bg-gradient-to-tl hover:from-gray-100/70 hover:to-white/70 hover:text-gray-900 hover:backdrop-blur-xl lg:py-3"
+								className="not-prose group inline-flex items-center gap-1 rounded-full border border-gray-200/20 bg-gradient-to-br from-purple-600/20 from-10% to-blue-600/20 px-8 py-2 text-base leading-none tracking-tight text-gray-200 shadow-lg backdrop-blur-lg transition duration-75 hover:border-gray-100/50 hover:bg-gradient-to-tl hover:from-gray-100/70 hover:to-white/70 hover:text-gray-900 hover:backdrop-blur-xl lg:py-3"
 								href="/docs/"
 							>
 								Get Started
 								<ChevronRightIcon
 									focusable="false"
-									className="transition-duration-150 group-hover:fill-gray-1000 inline h-6 transition group-hover:translate-x-1"
+									className="group-hover:fill-gray-1000 inline h-5 w-5 transition group-hover:translate-x-1"
 								/>
 							</Link>
 						</p>
