b1d7431132e77479eac4d9fbd89ad8704025ca55
Damon Cook <colorful-tones@users.noreply.github.com>
2024-10-31 12:58:16 -0400

    Add vertical margin to Blog, Showcase, Doc layout

--- src/components/docs-layout.js
+++ src/components/docs-layout.js
@@ -1,6 +1,6 @@
 export default function DocsLayout({ children }) {
 	return (
-		<div className="container-main container prose-lg prose-invert">
+		<div className="container-main container prose-lg prose-invert mb-24 mt-16 lg:mb-32 lg:mt-16">
 			{children}
 		</div>
 	);
