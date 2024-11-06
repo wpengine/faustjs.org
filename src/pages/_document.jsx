9c8a5f0730c7db7c1b29181d9152541421000be2
Kellen Mace <kellen.mace@gmail.com>
2024-11-06 13:34:52 -0500

    Add favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<link href="/images/favicon-32x32.png" rel="icon" sizes="32x32" />
+				<link href="/images/favicon-192x192.png" rel="icon" sizes="192x192" />
+			</Head>
 			<body className="bg-gray-900 text-gray-200">
 				<Main />
 				<NextScript />
