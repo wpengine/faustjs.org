de3b575eaa49c0e5007b9e5ab0c01a54e390d9e6
Alex Moon <moonmeister@users.noreply.github.com>
2024-11-06 15:12:03 -0800

    Update src/pages/index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -38,7 +38,7 @@ export default function Index() {
 							<Link
 								className="not-prose group inline-flex items-center gap-1 rounded-full border border-gray-200/20 bg-gradient-to-br from-purple-600/20 from-10% to-blue-600/20 px-8 py-2 text-base leading-none tracking-tight text-gray-200 shadow-lg backdrop-blur-lg transition duration-75 hover:border-gray-100/50 hover:bg-gradient-to-tl hover:from-gray-100/70 hover:to-white/70 hover:text-gray-900 hover:backdrop-blur-xl lg:py-3"
 								disableExternalIcon
-								href="https://discord.gg/headless-wordpress-836253505944813629"
+								href="/discord"
 								noDefaultStyles
 							>
 								Join the Discord
