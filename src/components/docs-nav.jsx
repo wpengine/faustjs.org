6178938148f2e40d9a71ec897d21bac3bddfd40c
Alex Moon <moonmeister@users.noreply.github.com>
2024-11-06 15:13:16 -0800

    wip

--- src/components/docs-nav.jsx
+++ src/components/docs-nav.jsx
@@ -1,17 +1,25 @@
+import { ChevronRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import Link from "@/components/link";
 import { classNames } from "@/utils/strings";
 
-export default function DocsNav({ routes, className }) {
+export default function DocsNav({ routes, className, level = 0 }) {
 	return (
-		<ul className={classNames("", className)}>
+		<ul
+			className={classNames("my-1", className, {
+				"ml-2 border-l-[.5px] border-gray-500": level > 0,
+			})}
+			data-doc-nav-level={level}
+		>
 			{routes.map((item) => (
-				<NavItem key={item.title} item={item} />
+				<NavItem item={item} key={item.title} level={level} />
 			))}
 		</ul>
 	);
 }
 
-function NavItem({ item, level = 0 }) {
+function NavItem({ item, level }) {
 	// If the item doesn't have a route, it's a heading
 	if (!item?.route) {
 		return (
@@ -21,12 +29,40 @@ function NavItem({ item, level = 0 }) {
 		);
 	}
 
+	if (!item.children) {
+		return (
+			<li>
+				<Link data-doc-nav-level={level} href={item.route} noDefaultStyles>
+					{item.title}
+				</Link>
+			</li>
+		);
+	}
+
+	return <DropdownNavItem item={item} level={level} />;
+}
+
+function DropdownNavItem({ item, level }) {
+	const router = useRouter();
+
+	const isCurrent = router.asPath.includes(item.route);
+
+	const [isOpen, setIsOpen] = useState(isCurrent);
+
 	return (
-		<li>
-			<Link href={item.route} data-doc-nav-level={level} noDefaultStyles>
-				{item.title}
+		<li className="my-2 font-light">
+			<Link data-doc-nav-level={level} href={item.route} noDefaultStyles>
+				<span>{item.title}</span>
+				{isOpen ? (
+					<ChevronDownIcon
+						className="inline h-4 w-4"
+						onClick={() => setIsOpen(!isOpen)}
+					/>
+				) : (
+					<ChevronRightIcon className="inline h-4 w-4" />
+				)}
 			</Link>
-			{item.children && <DocsNav routes={item.children} level={++level} />}
+			{isCurrent && <DocsNav level={++level} routes={item.children} />}
 		</li>
 	);
 }
