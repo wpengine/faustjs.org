e314035aeb83c8da7665178f4fd7d6e4827b950a
Alex Moon <moonmeister@users.noreply.github.com>
2024-11-12 13:09:41 -0800

    fix: errant js file duplicates of jsx

--- src/components/docs-layout.js
+++
@@ -1,7 +0,0 @@
-export default function DocsLayout({ children }) {
-	return (
-		<div className="container-main container prose-lg prose-invert mb-24 mt-16 lg:mb-32 lg:mt-16">
-			{children}
-		</div>
-	);
-}
