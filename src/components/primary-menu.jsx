4cd2035159704bb2982126dc0029b6a9bf0a1a61
Alex Moon <moonmeister@users.noreply.github.com>
2024-11-12 18:48:50 -0800

    cleanup ui

--- src/components/primary-menu.jsx
+++ src/components/primary-menu.jsx
@@ -20,7 +20,8 @@ const GET_PRIMARY_NAV = gql`
 	}
 `;
 
-const navItemClass = "text-gray-400 hover:text-gray-200";
+const navItemClass =
+	"text-gray-400 data-[focus]:text-purple-500 data-[focus]:outline rounded-md";
 
 export default function PrimaryMenu({ className }) {
 	const { data, loading, error } = useQuery(GET_PRIMARY_NAV);
@@ -34,16 +35,8 @@ export default function PrimaryMenu({ className }) {
 	const menuItems = data?.menu?.menuItems?.nodes || [];
 
 	return (
-		<Menu
-			as="nav"
-			className={classNames("flex items-center space-x-4", className)}
-		>
-			<MenuButton className="group rounded-md px-2 py-1.5 text-white/70 hover:text-white md:hidden">
-				<span className="sr-only">Open main nav</span>
-				<XMarkIcon className="z-20 hidden size-6 group-data-[open]:block" />
-				<Bars3Icon className="size-6 group-data-[open]:hidden" />
-			</MenuButton>
-			<ul className="hidden flex-row md:flex">
+		<nav className={classNames("flex items-center space-x-4", className)}>
+			<ul className="hidden flex-row space-x-4 pl-4 md:flex">
 				{menuItems.map((item) => (
 					<li key={item.databaseId} className={navItemClass}>
 						<Link className="block px-1" href={item.uri} noDefaultStyles>
@@ -52,19 +45,26 @@ export default function PrimaryMenu({ className }) {
 					</li>
 				))}
 			</ul>
-			<MenuItems
-				as="ul"
-				transition
-				className="absolute -left-4 top-[84.5px] flex w-full origin-top flex-col items-center justify-around gap-4 border-b-[.5px] border-gray-400 bg-gray-900/80 py-4 text-lg transition duration-200 ease-out data-[closed]:-translate-y-10 data-[closed]:opacity-0 md:hidden"
-			>
-				{menuItems.map((item) => (
-					<MenuItem key={item.databaseId} as="li" className={navItemClass}>
-						<Link className="block px-1" href={item.uri} noDefaultStyles>
-							{item.label}
-						</Link>
-					</MenuItem>
-				))}
-			</MenuItems>
-		</Menu>
+			<Menu>
+				<MenuButton className="group rounded-md px-2 py-1.5 text-white/70 hover:text-white md:hidden">
+					<span className="sr-only">Open main nav</span>
+					<XMarkIcon className="hidden size-6 group-data-[open]:block" />
+					<Bars3Icon className="size-6 group-data-[open]:hidden" />
+				</MenuButton>
+				<MenuItems
+					as="ul"
+					transition
+					className="container-blur-bg absolute -left-4 top-[84.5px] flex w-full origin-top flex-col items-center justify-around gap-4 border-b-[.5px] border-gray-400 bg-gray-900/80 py-4 text-lg transition duration-200 ease-out focus-within:outline-none data-[closed]:-translate-y-10 data-[closed]:opacity-0 md:hidden"
+				>
+					{menuItems.map((item) => (
+						<MenuItem key={item.databaseId} as="li" className={navItemClass}>
+							<a className="block px-1" href={item.uri}>
+								{item.label}
+							</a>
+						</MenuItem>
+					))}
+				</MenuItems>
+			</Menu>
+		</nav>
 	);
 }
