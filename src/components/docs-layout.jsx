0435758aad2db95b9ece75b709c20b983b8bfefd
Alex Moon <moonmeister@users.noreply.github.com>
2024-11-14 15:30:55 -0800

    wip

--- src/components/docs-layout.jsx
+++ src/components/docs-layout.jsx
@@ -1,42 +1,35 @@
 // import OnThisPageNav from "@/components/on-this-page-nav";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { ChevronRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
 import DocsNav from "@/components/docs-nav";
 import routes from "@/pages/docs/nav.json";
-import { classNames } from "@/utils/strings";
 
 export default function DocumentPage({ children }) {
-	const [isOpen, setIsOpen] = useState(false);
-
 	return (
 		<>
-			<div className="sticky top-[84px] border-b-[.5px] border-gray-500 bg-gray-900/80 backdrop-blur-sm lg:hidden">
-				<button
-					className={`group flex items-center px-2 py-1.5 text-white/70 hover:text-white ${isOpen ? "rounded-t-md bg-gray-800/80" : "rounded-md"}`}
-					onClick={() => setIsOpen(!isOpen)}
-					type="button"
-				>
-					{isOpen ? (
-						<ChevronDownIcon className="relative z-20 inline h-4 w-4" />
-					) : (
-						<ChevronRightIcon className="inline h-4 w-4" />
-					)}
+			<Menu
+				as="div"
+				className="sticky top-[84px] border-b-[.5px] border-gray-500 bg-gray-900/80 backdrop-blur-sm md:hidden"
+			>
+				<MenuButton className="flex items-center rounded-md px-2 py-1.5 text-white/70 hover:text-white data-[open]:rounded-t-md data-[open]:bg-gray-800/80">
+					<ChevronDownIcon className="relative z-20 hidden h-4 w-4 data-[open]:inline" />
+					<ChevronRightIcon className="inline h-4 w-4 data-[open]:hidden" />
 					<span className="pl-1">Menu</span>
-				</button>
-				<nav className={classNames({ hidden: !isOpen })}>
+				</MenuButton>
+				<MenuItems as="nav" className="hidden data-[open]:block">
+					<DocsNav as={MenuItem} routes={routes} />
+				</MenuItems>
+			</Menu>
+			<div className="mx-auto flex flex-col gap-6 md:max-w-screen-xl md:flex-row">
+				<nav className="hidden w-60 p-6 md:block">
 					<DocsNav routes={routes} />
 				</nav>
-			</div>
-			<div className="lg:max-w-8xl mx-auto flex flex-col gap-6 lg:flex-row">
-				<nav className="hidden w-60 p-6 lg:block">
-					<DocsNav routes={routes} />
-				</nav>
-				<main className="container-main lg:py-241 container prose prose-lg prose-invert px-8 py-14 lg:px-16">
+				<main className="container-main md:py-241 container prose prose-lg prose-invert px-8 py-14 md:px-16">
 					{children}
 				</main>
-				<section className="w-70 hidden p-6 min-[1100px]:block">
+				<nav className="w-70 hidden p-6 min-[1100px]:block">
 					{/* <OnThisPageNav editorBlocks={editorBlocks} /> */}
-				</section>
+				</nav>
 			</div>
 		</>
 	);
--- src/components/docs-nav.jsx
+++ src/components/docs-nav.jsx
@@ -1,10 +1,11 @@
 import { ChevronRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import Link from "@/components/link";
 import { classNames } from "@/utils/strings";
 
-export default function DocsNav({ routes, level = 0 }) {
+export default function DocsNav({ as, routes, level = 0 }) {
+	const As = as || Fragment;
 	return (
 		<ul
 			className={classNames("my-1", {
@@ -13,13 +14,15 @@ export default function DocsNav({ routes, level = 0 }) {
 			data-doc-nav-level={level}
 		>
 			{routes.map((item) => (
-				<NavItem item={item} key={item.title} level={level} />
+				<As key={item.title}>
+					<NavItem item={item} level={level} />
+				</As>
 			))}
 		</ul>
 	);
 }
 
-function NavItem({ item, level }) {
+function NavItem({ item, level, ...props }) {
 	// If the item doesn't have a route, it's a heading
 	if (!item?.route) {
 		return (
@@ -29,40 +32,46 @@ function NavItem({ item, level }) {
 		);
 	}
 
-	if (!item.children) {
-		return (
-			<li>
-				<Link data-doc-nav-level={level} href={item.route} noDefaultStyles>
-					{item.title}
-				</Link>
-			</li>
-		);
-	}
-
-	return <DropdownNavItem item={item} level={level} />;
-}
-
-function DropdownNavItem({ item, level }) {
-	const router = useRouter();
-
-	const isCurrent = router.asPath.includes(item.route);
-
-	const [isOpen, setIsOpen] = useState(isCurrent);
-
 	return (
-		<li className="my-2 font-light">
-			<Link data-doc-nav-level={level} href={item.route} noDefaultStyles>
-				<span>{item.title}</span>
-				{isOpen ? (
-					<ChevronDownIcon
-						className="inline h-4 w-4"
-						onClick={() => setIsOpen(!isOpen)}
-					/>
-				) : (
-					<ChevronRightIcon className="inline h-4 w-4" />
-				)}
+		<li>
+			<Link
+				data-doc-nav-level={level}
+				href={item.route}
+				noDefaultStyles
+				{...props}
+			>
+				{item.title}
 			</Link>
-			{isCurrent && <DocsNav level={level + 1} routes={item.children} />}
 		</li>
 	);
 }
+
+// function DropdownNavItem({ item, level, ...props }) {
+// 	const router = useRouter();
+
+// 	const isCurrent = router.asPath.includes(item.route);
+
+// 	const [isOpen, setIsOpen] = useState(isCurrent);
+
+// 	return (
+// 		<li className="my-2 font-light">
+// 			<Link
+// 				data-doc-nav-level={level}
+// 				href={item.route}
+// 				noDefaultStyles
+// 				{...props}
+// 			>
+// 				<span>{item.title}</span>
+// 				{isOpen ? (
+// 					<ChevronDownIcon
+// 						className="inline h-4 w-4"
+// 						onClick={() => setIsOpen(!isOpen)}
+// 					/>
+// 				) : (
+// 					<ChevronRightIcon className="inline h-4 w-4" />
+// 				)}
+// 			</Link>
+// 			{isCurrent && <DocsNav level={level + 1} routes={item.children} />}
+// 		</li>
+// 	);
+// }
