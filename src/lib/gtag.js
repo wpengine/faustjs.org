b7379917b1fb237d4bb297172d62a94d95c61315
Alex Moon <moonmeister@users.noreply.github.com>
2024-11-26 09:09:40 -0800

    fix: next doesn't like node:prefix

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,6 +1,6 @@
-import { env } from "node:process";
+import process from "process"; // eslint-disable-line unicorn/prefer-node-protocol
 
-export const GA_TRACKING_ID = env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const logPageview = (url) => {
