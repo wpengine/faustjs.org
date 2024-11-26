202a606f475dc105e07ff8fc67ec50a392b34bfa
Alex Moon <moonmeister@users.noreply.github.com>
2024-11-26 15:30:24 -0800

    move to globalThis

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -4,7 +4,7 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const logPageview = (url) => {
-	window.gtag("config", GA_TRACKING_ID, {
+	globalThis.gtag("config", GA_TRACKING_ID, {
 		page_path: url,
 	});
 };
