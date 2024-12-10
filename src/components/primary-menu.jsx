c6682ae6db4cfffd19504221f04879dca67309ec
Colin Murphy <colin.murphy@wpengine.com>
2024-12-10 10:56:04 +0000

    Replaced primary menu

--- src/components/primary-menu.jsx
+++ src/components/primary-menu.jsx
@@ -1,25 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { forwardRef } from "react";
 import Link from "@/components/link";
+import { HeaderMenu } from "@/constants/menus";
 import { classNames } from "@/utils/strings";
-
-const GET_PRIMARY_NAV = gql`
-	query GetPrimaryNav {
-		menu(id: "primary-nav", idType: NAME) {
-			id
-			name
-			menuItems {
-				nodes {
-					label
-					uri
-					databaseId
-				}
-			}
-		}
-	}
-`;
 
 const navItemClass =
 	"text-gray-400 data-[focus]:text-purple-500 data-[focus]:outline rounded-md px-1";
@@ -33,21 +17,11 @@ const CustomLink = forwardRef((props, reference) => {
 });
 
 export default function PrimaryMenu({ className }) {
-	const { data, loading, error } = useQuery(GET_PRIMARY_NAV);
-
-	if (loading) return <p>Loading...</p>;
-	if (error) {
-		console.error("Error loading navigation items:", error);
-		return <p>Error loading navigation items.</p>;
-	}
-
-	const menuItems = data?.menu?.menuItems?.nodes || [];
-
 	return (
 		<nav className={classNames("flex items-center space-x-4", className)}>
 			<ul className="hidden flex-row space-x-4 pl-4 md:flex">
-				{menuItems.map((item) => (
-					<li key={item.databaseId} className={navItemClass}>
+				{HeaderMenu.map((item) => (
+					<li key={item.id} className={navItemClass}>
 						<Link className="block px-1" href={item.uri} noDefaultStyles>
 							{item.label}
 						</Link>
@@ -70,10 +44,10 @@ export default function PrimaryMenu({ className }) {
 					transition
 					className="container-blur-bg absolute -left-4 top-[84.5px] flex w-full origin-top flex-col items-center justify-around gap-4 border-b-[.5px] border-gray-400 bg-gray-900/80 py-4 text-lg transition duration-200 ease-out focus-within:outline-none data-[closed]:-translate-y-10 data-[closed]:opacity-0 md:hidden"
 				>
-					{menuItems.map((item) => (
+					{HeaderMenu.map((item) => (
 						<MenuItem
 							as={CustomLink}
-							key={item.databaseId}
+							key={item.id}
 							className={navItemClass}
 							noDefaultStyles
 							href={item.uri}
--- src/constants/menus.js
+++ src/constants/menus.js
@@ -1,2 +1,20 @@
 export const PRIMARY_LOCATION = "PRIMARY";
 export const FOOTER_LOCATION = "FOOTER_1";
+
+export const HeaderMenu = [
+	{
+		id: "docs",
+		uri: "/docs/",
+		label: "Docs",
+	},
+	{
+		id: "blog",
+		uri: "/blog/",
+		label: "Blog",
+	},
+	{
+		id: "showcase",
+		uri: "/showcase/",
+		label: "Showcase",
+	},
+];
