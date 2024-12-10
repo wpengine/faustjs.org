8f8725307c733f5d4c1b7de5779eabd2c209919a
Colin Murphy <colin.murphy@wpengine.com>
2024-12-10 17:43:31 +0000

    Hardcoded header links

--- src/components/primary-menu.jsx
+++ src/components/primary-menu.jsx
@@ -2,7 +2,6 @@ import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { forwardRef } from "react";
 import Link from "@/components/link";
-import { HeaderMenu } from "@/constants/menus";
 import { classNames } from "@/utils/strings";
 
 const navItemClass =
@@ -20,13 +19,21 @@ export default function PrimaryMenu({ className }) {
 	return (
 		<nav className={classNames("flex items-center space-x-4", className)}>
 			<ul className="hidden flex-row space-x-4 pl-4 md:flex">
-				{HeaderMenu.map((item) => (
-					<li key={item.id} className={navItemClass}>
-						<Link className="block px-1" href={item.uri} noDefaultStyles>
-							{item.label}
-						</Link>
-					</li>
-				))}
+				<li key="docs" className={navItemClass}>
+					<Link className="block px-1" href="/docs/" noDefaultStyles>
+						Docs
+					</Link>
+				</li>
+				<li key="blog" className={navItemClass}>
+					<Link className="block px-1" href="/blog/" noDefaultStyles>
+						Blog
+					</Link>
+				</li>
+				<li key="showcase" className={navItemClass}>
+					<Link className="block px-1" href="/showcase/" noDefaultStyles>
+						Showcase
+					</Link>
+				</li>
 			</ul>
 			<Menu>
 				<MenuButton className="group rounded-md px-2 py-1.5 text-white/70 hover:text-white md:hidden">
@@ -44,17 +51,33 @@ export default function PrimaryMenu({ className }) {
 					transition
 					className="container-blur-bg absolute -left-4 top-[84.5px] flex w-full origin-top flex-col items-center justify-around gap-4 border-b-[.5px] border-gray-400 bg-gray-900/80 py-4 text-lg transition duration-200 ease-out focus-within:outline-none data-[closed]:-translate-y-10 data-[closed]:opacity-0 md:hidden"
 				>
-					{HeaderMenu.map((item) => (
-						<MenuItem
-							as={CustomLink}
-							key={item.id}
-							className={navItemClass}
-							noDefaultStyles
-							href={item.uri}
-						>
-							{item.label}
-						</MenuItem>
-					))}
+					<MenuItem
+						as={CustomLink}
+						key="docs"
+						className={navItemClass}
+						noDefaultStyles
+						href="/docs/"
+					>
+						Docs
+					</MenuItem>
+					<MenuItem
+						as={CustomLink}
+						key="blog"
+						className={navItemClass}
+						noDefaultStyles
+						href="/blog/"
+					>
+						Blog
+					</MenuItem>
+					<MenuItem
+						as={CustomLink}
+						key="showcase"
+						className={navItemClass}
+						noDefaultStyles
+						href="/showcase/"
+					>
+						Showcase
+					</MenuItem>
 				</MenuItems>
 			</Menu>
 		</nav>
--- src/constants/menus.js
+++ src/constants/menus.js
@@ -1,24 +1,6 @@
 export const PRIMARY_LOCATION = "PRIMARY";
 export const FOOTER_LOCATION = "FOOTER_1";
 
-export const HeaderMenu = [
-	{
-		id: "docs",
-		uri: "/docs/",
-		label: "Docs",
-	},
-	{
-		id: "blog",
-		uri: "/blog/",
-		label: "Blog",
-	},
-	{
-		id: "showcase",
-		uri: "/showcase/",
-		label: "Showcase",
-	},
-];
-
 export const FooterMenuSections = {
 	downloads: "Downloads",
 	community: "Community",
