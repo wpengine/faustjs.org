8f35ffc17a03f30654f9fea6980167f3e28efc0d
Fran Agulto <francis.agulto@wpengine.com>
2025-01-30 16:49:26 -0600

    pulled in toolkit updates and added a redirect

--- next.config.mjs
+++ next.config.mjs
@@ -31,6 +31,11 @@ const nextConfig = {
 				permanent: true,
 			},
 			{
+				source: "guide/how-to-customize-the-toolbar",
+				destination: "/docs/how-to/customize-the-toolbar/",
+				permanent: true,
+			},
+			{
 				source: "/guide/how-to-create-a-plugin",
 				destination: "/docs/how-to/create-a-plugin/",
 				permanent: true,
