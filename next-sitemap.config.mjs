67b0f66460112dbdf5d116c31ca920e9a8468009
Joe Fusco <joe.fusco@wpengine.com>
2025-02-20 13:52:23 -0500

    Update sitemap config

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,15 +1,16 @@
 import { env } from "node:process";
+import { URL } from "node:url";
 
 const SITE_URL = env.NEXT_PUBLIC_SITE_URL;
 
 export default {
 	siteUrl: SITE_URL,
 	generateRobotsTxt: true,
-	exclude: ["/wp-sitemap.xml"],
+	exclude: ["/wp-sitemap.xml"], // <= exclude here
 
 	robotsTxtOptions: {
 		additionalSitemaps: [
-			`${SITE_URL}/wp-sitemap.xml`,
+			new URL("/wp-sitemap.xml", SITE_URL), // <==== Add here
 		],
 		policies: [
 			{
