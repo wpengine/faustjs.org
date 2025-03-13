05bc823407aa6c8c8d3b947b21c18abaaced4cf8
Alex Moon <moonmeister@users.noreply.github.com>
2025-03-13 07:20:52 -0700

    remove todo

--- src/constants/repo.mjs
+++ src/constants/repo.mjs
@@ -1,5 +1,4 @@
 export const DOCS_OWNER = "wpengine";
 export const DOCS_REPO = "faustjs";
 export const DOCS_FOLDER = "docs";
-// @TODO: Change to `main/canary` for production
 export const DOCS_BRANCH = "canary";
