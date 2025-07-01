706e91f79883019cfe6c40eac280c528f4921d29
Alex Moon <moonmeister@users.noreply.github.com>
2025-07-01 10:25:41 -0700

    refactor: switch to more semantic aside

--- src/components/docs-recommendations.jsx
+++ src/components/docs-recommendations.jsx
@@ -38,7 +38,7 @@ export default function DocsRecommended({ docID, count = 5 }) {
 	}, [inView, fetchRecommendations]);
 
 	return (
-		<div
+		<aside
 			ref={ref}
 			className="docs-recommended bg-blue-1100/20 rounded-lg p-6 text-white shadow-lg ring-1 ring-blue-500/10"
 		>
@@ -75,6 +75,6 @@ export default function DocsRecommended({ docID, count = 5 }) {
 					))}
 				</ul>
 			)}
-		</div>
+		</aside>
 	);
 }
