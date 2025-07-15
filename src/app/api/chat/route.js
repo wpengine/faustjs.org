1c3c72836b6950c63409e74a43d5b4e7818d2e6b
Alex Moon <moonmeister@users.noreply.github.com>
2025-07-15 12:50:18 -0700

    Prompt for more detailed links and information

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -41,14 +41,19 @@ const vertex = createVertex({
 
 const smartSearchPrompt = `
     - You can use the 'smartSearchTool' to find information relating to Faust.
-      - WP Engine Smart Search is a powerful tool for finding information about Faust.
-      - After the 'smartSearchTool' provides results (even if it's an error or no information found)
-      - You MUST then formulate a conversational response to the user based on those results but also use the tool if the users query is deemed plausible.
-        - If search results are found, summarize them for the user.
-        - If no information is found or an error occurs, inform the user clearly.`;
+    - WP Engine Smart Search is a powerful tool for finding information about Faust.
+  	- After the 'smartSearchTool' provides results (even if it's an error or no information found)
+  	- You MUST then formulate a conversational response to the user based on those results but also use the tool if the users query is deemed plausible.
+      - If search results are found, summarize them for the user.
+      - If no information is found or an error occurs, inform the user clearly.
+		- All links from this tool are root relative to the Faust documentation site (https://faustjs.org).`;
 
 const systemPromptContent = `
-    - You are a friendly and helpful AI assistant
+    - You are a friendly and helpful AI assistant.
+    - Format your responses using Github Flavored Markdown.
+    - Make sure to format links as [link text](path).
+		- Make sure to clink out to the source of the information you provide.
+		- Prefer new information over old information.
     - Do not invent information. Stick to the data provided by the tool.`;
 
 export async function POST(req) {
